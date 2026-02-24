aaf951cc4ab00322fa48a6f84c2d532ba69decf8
GeotrendsCo <gerencia@geotrends.co>
2026-02-24 16:36:14 -0500

    Geodatos

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 // Middlewares básicos
 app.use(express.json());
